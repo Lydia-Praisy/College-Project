643f49f7d52b5d26d85b6c80d4a38c06e36a061f
Lydia-Praisy <89208127+Lydia-Praisy@users.noreply.github.com>
2021-08-19 22:42:56 +0530

    Getting sem number from user

--- CgpaCalculator.c
+++ CgpaCalculator.c
@@ -1,1 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int sem;
 
+printf("Enter the semester:");
+scanf("%d",&sem);
+if((sem < 1) || (sem > 8))
+{
+printf("Invalid semester ");
+return 0;
+}
+}
