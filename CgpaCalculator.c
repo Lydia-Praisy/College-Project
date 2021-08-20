01d101a78e4eef761b1a4ff0c542699315aee3ef
Lydia-Praisy <89208127+Lydia-Praisy@users.noreply.github.com>
2021-08-20 22:15:53 +0530

    Update CgpaCalculator.c

--- CgpaCalculator.c
+++ CgpaCalculator.c
@@ -3,12 +3,18 @@
 int main()
 {
 int sem;
-
-printf("Enter the semester:");
+printf("\t\t\t\t\t\tCGPA calculator");
+printf("\n\nEnter the semester:");
 scanf("%d",&sem);
 if((sem < 1) || (sem > 8))
 {
 printf("Invalid semester ");
 return 0;
 }
+printf("\t\t\t\t\t\t General Instruction");
+printf("\n\n\n If grade is O - Press 1");
+printf("\n\n If grade is A+ - Press 2");
+printf("\n\n If grade is A - Press 3");
+printf("\n\n If grade is B+ - Press 4");
+printf("\n\n If grade is B - Press 5");
 }
